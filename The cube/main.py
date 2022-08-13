1f15091626cca8887477ce032225f6fa502d67cc
Master <110470276+masterr13@users.noreply.github.com>
2022-08-13 20:35:30 +0300

    Update main.py

--- The cube/main.py
+++ The cube/main.py
@@ -1,20 +1,16 @@
 ﻿import pygame
 import math
 import sys
-
 pygame.init()
 
 sc = pygame.display.set_mode((1200, 700))
 pygame.display.set_caption("Huterr")
 
 player = pygame.image.load("PL1.png")
-Cube = pygame.image.load("Stena.png")
-Gun = pygame.image.load("Drobash.png")
 bg = pygame.image.load("background.jpeg")
 
 # player
-x = 148
-y = 550
+jump = 4
 speed = 5
 
 #gun
@@ -24,33 +20,25 @@ Gy = 552
 clock = pygame.time.Clock()
 
 run = True
-
+correction_angle = 90
 while(run):
     clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    #Controll Cube
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        x += speed
-        print("1")
-    elif keys[pygame.K_a]:
-        x -= speed
-        print("2")
-    elif keys[pygame.K_w]:
-        y -= speed
-        print("3")
-    elif keys[pygame.K_s]:
-        y += speed
-        print("0")
-    if keys[pygame.K_ESCAPE]:
-        exit()
+    player_pos  = sc.get_rect().center
+    player_rect = player.get_rect(center = player_pos)
 
-    sc.blit(bg, (0, 0))
-    sc.blit(Gun,(Gx, Gy))
-    sc.blit(player, (x, y))
+    mx, my = pygame.mouse.get_pos()
+    dx, dy = mx - player_rect.centerx, my - player_rect.centery
+    angle = math.degrees(math.atan2(-dy, dx)) - correction_angle
+
+    rot_image = pygame.transform.rotate(player, angle)
+    rot_image_rect = rot_image.get_rect(center = player_rect.center)
+
+    sc.fill((55, 55, 55))
+    sc.blit(rot_image, rot_image_rect.topleft)
+    pygame.display.flip()
     pygame.display.update()
-
-pygame.quit()
+    pygame.quit
